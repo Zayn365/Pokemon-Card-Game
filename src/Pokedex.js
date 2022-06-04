bd124165f1d5d5018f59f55b2fc7be012aaa81b7
DESKTOP-PH4940M\Petrol Solutions <zaynsaeed365@gmail.com>
2022-06-04 13:21:01 +0500

    update

---
+++ src/Pokedex.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+class Pokedex extends Component {
+   render () {
+       return(
+           <div className="Body">
+              <h1 className="Main">Pokedex Game</h1>
+           </div>
+       )
+   }
+
+}
+
+
+export default Pokedex 
